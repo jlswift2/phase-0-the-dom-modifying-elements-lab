9d3194eb49fc4021dc32efbb4f6b40626ba1e335
Justin Swift <jlswift2@gmail.com>
2022-01-19 16:33:20 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.className = "victory"
+newHeader.id = 'victory'
+newHeader.textContent = "Justin Swift is the champion"
+document.body.append(newHeader)
+
